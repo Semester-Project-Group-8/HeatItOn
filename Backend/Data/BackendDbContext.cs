eb7f65199d354120806ea7a2ca5488ef8f518277
FrenchSpeedBump <alex.zalanyi21@gmail.com>
2026-02-27 08:50:17 +0100

    added assets images and results to backend db context

--- Backend/Data/BackendDbContext.cs
+++ Backend/Data/BackendDbContext.cs
@@ -5,6 +5,9 @@ namespace Backend.Data
     {
         public BackendDbContext(DbContextOptions options) : base(options) { }
         public DbSet<Models.Demand> Demands { get; set; }
+        public DbSet<Models.Asset> Assets{ get; set; }
+        public DbSet<Models.Image> Images { get; set; }
+        public DbSet<Models.Result> Results{ get; set; }
 
 
     }
