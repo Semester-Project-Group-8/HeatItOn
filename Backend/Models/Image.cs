13bb7389f14e62cd355b9d2a6beb956cf13747b8
FrenchSpeedBump <alex.zalanyi21@gmail.com>
2026-02-27 08:45:45 +0100

    added assets image and result

---
+++ Backend/Models/Image.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Image
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ImageLink { get; set; }
+    }
+}
---
+++ Backend/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Result
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public float HeatProduction { get; set; }
+        [Required]
+        public float Electricity { get; set; }
+        [Required]
+        public float ProductionCost { get; set; }
+        [Required]
+        public float PrimaryEnergyConsumed { get; set; }
+        [Required]
+        public int CO2Produced { get; set; }
+        [Required]
+        public int AssetId { get; set; }
+        [Required]
+        public Asset Asset { get; set; }
+    }
+}
