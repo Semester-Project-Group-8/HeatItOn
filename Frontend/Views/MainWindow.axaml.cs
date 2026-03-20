d3d38aade59a7c4236f364f855a326ca757470c5
Zolizoli110 <zolika.suveges@gmail.com>
2026-03-20 12:38:34 +0100

    updated namespaces

--- Frontend/Views/MainWindow.axaml.cs
+++ Frontend/Views/MainWindow.axaml.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Backend.Models;
 using Frontend.Data;
+using Frontend.Models;
 
 namespace Frontend.Views;
 
