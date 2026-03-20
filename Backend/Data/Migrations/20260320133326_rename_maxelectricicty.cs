f440ec545ebe1ae4d81c08b41d1d4a4a9b76543a
Zolizoli110 <zolika.suveges@gmail.com>
2026-03-20 22:38:19 +0100

    migrate

---
+++ Backend/Data/Migrations/20260320133326_rename_maxelectricicty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename_maxelectricicty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaxElectricicty",
+                table: "Assets",
+                newName: "MaxElectricity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaxElectricity",
+                table: "Assets",
+                newName: "MaxElectricicty");
+        }
+    }
+}
