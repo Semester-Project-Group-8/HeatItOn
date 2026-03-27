8771e85753f662f3aec616bfff7af3f23b4246a1
Zolizoli110 <zolika.suveges@gmail.com>
2026-03-27 11:33:03 +0100

    changed files

---
+++ Frontend/ViewModels/TestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Frontend.Data;
+
+namespace Frontend.ViewModels;
+
+public partial class TestViewModel : ViewModelBase
+{
+    public readonly AssetClient AssetClient;
+    public readonly SourceClient SourceClient;
+    public TestViewModel(AssetClient assetClient, SourceClient sourceClient)
+    {
+        AssetClient = assetClient;
+        SourceClient = sourceClient;
+    }
+}
