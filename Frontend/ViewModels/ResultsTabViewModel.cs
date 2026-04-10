02b82d5018544137111bec6f2606777d26f3e102
Filip Tichý <filiptichy888@gmail.com>
2026-04-10 10:08:52 +0200

    HEAT-104: Add scrollable dropdown to Results tab with backend integration
    - Added scrollable ComboBox (MaxDropDownHeight=250) to display result list IDs
    - Integrated ResultsTabViewModel with HttpClient connecting to backend at localhost:8080
    - Added error handling in LoadResultLists and LoadResultList methods
    - Wired ResultsTabView to ViewModel in code-behind

--- Frontend/ViewModels/ResultsTabViewModel.cs
+++ Frontend/ViewModels/ResultsTabViewModel.cs
@@ -1,6 +1,87 @@
+using Frontend.Data;
+using Frontend.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Frontend.ViewModels;
 
 public class ResultsTabViewModel : ViewModelBase
 {
-    
+    private readonly ResultClient _resultClient;
+    private readonly ResultListClient _resultListClient;
+    private readonly OptimizerClient _optimizerClient;
+
+    private List<int>? _resultLists;
+    public List<int>? ResultLists
+    {
+        get => _resultLists;
+        set
+        {
+            _resultLists = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int? _selectedResultListId;
+    public int? SelectedResultListId
+    {
+        get => _selectedResultListId;
+        set
+        {
+            _selectedResultListId = value;
+            OnPropertyChanged();
+            _ = LoadResultList();
+        }
+    }
+
+    private ResultList? _currentResultList;
+    public ResultList? CurrentResultList
+    {
+        get => _currentResultList;
+        set
+        {
+            _currentResultList = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ResultsTabViewModel(OptimizerClient optimizerClient, ResultClient resultClient, ResultListClient resultListClient)
+    {
+        _optimizerClient = optimizerClient;
+        _resultClient = resultClient;
+        _resultListClient = resultListClient;
+
+        _ = LoadResultLists();
+    }
+
+    public async Task LoadResultLists()
+    {
+        try
+        {
+            ResultLists = await _resultListClient.ListResultLists() ?? new List<int>();
+        }
+        catch
+        {
+            ResultLists = new List<int>();
+        }
+    }
+
+    public async Task LoadResultList()
+    {
+        if (SelectedResultListId is null)
+        {
+            CurrentResultList = null;
+            return;
+        }
+
+        try
+        {
+            CurrentResultList = await _resultListClient.GetResultListById(SelectedResultListId.Value);
+        }
+        catch
+        {
+            CurrentResultList = null;
+        }
+    }
+
 }
--- Frontend/Views/ResultsTabView.axaml.cs
+++ Frontend/Views/ResultsTabView.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Frontend.Data;
+using Frontend.ViewModels;
+using System;
+using System.Net.Http;
 
 namespace Frontend.Views;
 
@@ -7,5 +11,15 @@ public partial class ResultsTabView : UserControl
     public ResultsTabView()
     {
         InitializeComponent();
+
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:8080/")
+        };
+
+        DataContext = new ResultsTabViewModel(
+            new OptimizerClient(httpClient),
+            new ResultClient(httpClient),
+            new ResultListClient(httpClient));
     }
 }
